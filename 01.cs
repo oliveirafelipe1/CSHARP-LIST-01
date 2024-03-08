03c16d030bd625037a2defe3481d17ced66e8974
Felipe Oliveira <161166657+oliveirafelipe1@users.noreply.github.com>
2024-03-08 19:45:41 -0300

    Add files via upload

---
+++ 01.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Digite um numero: ");
+            int numero1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite outro numero: ");
+            int numero2 = int.Parse(Console.ReadLine());
+
+            if (numero1 > numero2)
+                Console.WriteLine($"O {numero1} é maior que o {numero2}");
+
+            else if (numero2 > numero1)
+                Console.WriteLine($"O {numero2} é maior que o {numero1}");
+
+            else
+                Console.WriteLine("Os dois número são iguais.");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 02.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um número: ");
+            double numero = double.Parse(Console.ReadLine());
+
+            if (numero > 0)
+            {
+                double resultado = Math.Sqrt(numero);
+                Console.WriteLine("A raiz quadrada do seu número é: " + resultado);
+            }
+            else
+            {
+                Console.WriteLine("O seu número é inválido.");
+            }
+            Console.ReadLine();
+        }
+    }
+}
