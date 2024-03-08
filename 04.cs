db0ad37ac07698f3f6359a459a4974a7061aa3b1
Felipe Oliveira <161166657+oliveirafelipe1@users.noreply.github.com>
2024-03-08 20:00:56 -0300

    Add files via upload

---
+++ 04.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um número: ");
+            double numero = double.Parse(Console.ReadLine());
+
+            if (numero > 0)
+            {
+                double numeroAoQuadrado = numero * numero;
+                double raizQuadrada = Math.Sqrt(numero);
+                Console.WriteLine($"O seu numero ao quadrado é {numeroAoQuadrado} e a raíz {raizQuadrada}. ");
+            }
+            else
+            {
+                Console.WriteLine("Seu número precisa ser positivo. ");
+            }
+            Console.ReadLine();
+        }
+    }
+}
