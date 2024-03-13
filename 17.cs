cfc46173b8147835b0e5d312b3458b7a09270914
Felipe Oliveira <161166657+oliveirafelipe1@users.noreply.github.com>
2024-03-13 20:17:49 -0300

    Add files via upload

---
+++ 17.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float areaTrapezio;
+
+            Console.WriteLine("Digite o valor da base menor: ");
+            float baseMenor = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o valor da base maior: ");
+            float baseMaior= float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o valor da altura: ");
+            float altura = float.Parse(Console.ReadLine());
+
+            float resultado1 = (baseMaior + baseMenor) * altura;
+            areaTrapezio = resultado1 / 2;
+
+            if (baseMenor > 0 && baseMaior > 0)
+            {
+                Console.WriteLine("O valor da área do seu trapezio é de: " + areaTrapezio + "m²");
+            }
+            else
+            {
+                Console.WriteLine("O seu trapezio precisa de bases com um valor maior que zero.");
+            }
+            Console.ReadLine();
+        }
+    }
+}
