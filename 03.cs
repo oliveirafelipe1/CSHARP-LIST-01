918fa0a4ced1ac87eadfda5f52632d071f67bac4
Felipe Oliveira <161166657+oliveirafelipe1@users.noreply.github.com>
2024-03-22 14:52:50 -0300

    Add files via upload

---
+++ 03.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um numero real: ");
+            int numeroReal = int.Parse(Console.ReadLine());
+
+            if (numeroReal >= 0)
+            {
+                double resultado1 = Math.Sqrt(numeroReal);
+                Console.WriteLine("A raiz quadrada do seu número é: " + resultado1);
+            }
+            else
+            {
+                double resultado2 = numeroReal * numeroReal;
+                Console.WriteLine("O seu numero elevado a ele mesmo da: " + resultado2);
+            }
+            Console.ReadLine();
+        }
+    }
+}
