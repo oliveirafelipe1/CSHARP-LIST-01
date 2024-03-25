62f58ae488caf2e4bf3cdba3248406f480ddb829
Felipe Oliveira <161166657+oliveirafelipe1@users.noreply.github.com>
2024-03-25 20:28:29 -0300

    Add files via upload

---
+++ 12.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp46
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um número inteiro: ");
+            int numero = int.Parse(Console.ReadLine());
+
+            if (numero <= 0) 
+            {
+                Console.WriteLine("O seu numero precisa ser maior que zero.");
+            }
+            else
+            {
+                double soma = Math.Log10(numero);
+                Console.WriteLine("O logaritmo do seu número é de: " + soma);
+            }
+            Console.ReadLine();
+        }
+    }
+}
