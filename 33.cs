cba29827adc350f60c709e16f28b6f7f3d00a9d1
Felipe Oliveira <161166657+oliveirafelipe1@users.noreply.github.com>
2024-03-25 20:48:15 -0300

    Add files via upload

---
+++ 33.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp78
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o preço antigo do seu produto: ");
+            double produto = double.Parse(Console.ReadLine());
+
+            double resultado = 0;
+
+            if (produto < 50)
+            {
+                resultado = produto + produto * 5 / 100;
+                Console.WriteLine("O preço novo do seu produto é: " + resultado);
+            }
+            else if (produto >= 50 && produto <= 100)
+            {
+                resultado = produto + produto * 10 / 100;
+                Console.WriteLine("O preço novo do seu produto é: " + resultado);
+            }
+            else if (produto > 100)
+            {
+                resultado = produto + produto * 15 / 100;
+                Console.WriteLine("O preço novo do seu produto é: " + resultado);
+            }
+            else
+            {
+                Console.WriteLine("Preço inválido!");
+            }
+            Console.ReadLine();
+        }
+    }
+}
